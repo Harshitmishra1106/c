d147c6afb2c7c891f07434aced6c458a18c21e7d
Harshitmishra1106 <96685336+Harshitmishra1106@users.noreply.github.com>
2021-12-31 20:33:49 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,sum,sub,mul,div;
+    printf("enter first numbers \t");
+    scanf("%d",&a);
+    printf("enter Second numbers \t");
+    scanf("%d",&b);
+    sum=(a+b);
+    sub=(a-b);
+    mul=(a*b);
+    div=(a/b);
+    printf("\nthe sum of the numbers is =%d",sum);
+    printf("\nthe sub of the numbers is =%d",sub);
+    printf("\nthe mul of the numbers is =%d",mul);
+    printf("\nthe div of the numbers is =%d",div);
+}
