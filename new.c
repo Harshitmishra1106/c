d8c766d532cf3467083d5248257481f351953193
Harshitmishra1106 <96685336+Harshitmishra1106@users.noreply.github.com>
2022-01-07 21:52:57 +0530

    Add files via upload

---
+++ new.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int n,m;
+    printf("enter a five-digit no.\n");
+    scanf("%d",&n);
+    m=n+11111;
+    printf("%d is the result the question asked us",m);
+}
