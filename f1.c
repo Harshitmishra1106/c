9ad0e568f2a495c8366d9f6fa33c3490ea9f38aa
Harshitmishra1106 <96685336+Harshitmishra1106@users.noreply.github.com>
2022-01-13 23:39:19 +0530

    Add files via upload

---
+++ f1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int power(int ,int );
+int main()
+{
+    int a,b,c;
+    printf("enter a and b to get a raised to the power b");
+    scanf("%d%d",&a,&b);
+    c= power(int ,int );
+    printf("%d is the result",c);
+    return 0;
+}
+int power(int ,int )
+{
+    int m,n,x;
+    x=pow(m,n);
+    return x;
+}
