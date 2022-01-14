34b1c878c92cd23de48b1f7daed8df0885343a87
Harshitmishra1106 <96685336+Harshitmishra1106@users.noreply.github.com>
2022-01-14 23:44:56 +0530

    Add files via upload

---
+++ call1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void print();
+void main()
+{
+    int a[5],i;
+    printf("enter the array elements");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    void print();
+}
+void print()
+{
+    int a[5],i;
+    for(i=0;i<5;i++)
+    {
+        printf("%d",a[i]);
+    }
+}
