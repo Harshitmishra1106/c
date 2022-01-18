1d8fcd906d58642a03981eb4183ba40034fb7b25
Harshitmishra1106 <96685336+Harshitmishra1106@users.noreply.github.com>
2022-01-18 22:28:06 +0530

    Add files via upload

---
+++ b6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    int c;
+    printf("enter the value of a and b\n");
+    scanf("%f %f",&a,&b);
+    c=a/b;
+    printf("%f is the result",(float)c);
+}
